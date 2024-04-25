56accb90d260ed60967dfefed497a956c3ae4863
Palash Dhavle <palashdhavle15@gmail.com>
2024-04-25 15:44:17 +0530

    chore: fixed some type errors in Meter component

--- src/components/Meter.tsx
+++ src/components/Meter.tsx
@@ -1,14 +1,24 @@
+// biome-ignore lint/style/useImportType: <explanation>
 import React from "react";
 
+type Props = {
+	percentage: number,
+	size:number,
+	importance: number,
+	conincStart?: number,
+	conicDiff?: number,
+	gap?: number,
+	children: React.ReactNode;
+}
 function Meter({
 	percentage,
 	size,
-	importance = "4",
+	importance = 4,
 	conincStart = 80,
 	conicDiff = 5,
 	gap = 5,
 	children,
-}) {
+}: Props) {
 	const importance_map = {
 		1: "#F6E5D9",
 		2: "#FAF4BE",
@@ -26,14 +36,14 @@ function Meter({
 			className="group rounded-full flex justify-center items-center"
 			style={{
 				background: `radial-gradient(closest-side, ${
-					importance_map[importance]
+					importance_map[importance as keyof typeof importance_map]
 				} ${conincStart}%, transparent ${conincStart + conicDiff}% 100%), conic-gradient(${
 					Number(percentage) >= 75
 						? per_colors.excelent
 						: Number(percentage) >= 30
 							? per_colors.good
 							: per_colors.bad
-				} ${percentage}%, ${importance_map[importance]}  0)`,
+				} ${percentage}%, ${importance_map[importance as keyof typeof importance_map]}  0)`,
 				width: size,
 				height: size,
 				padding: gap,
