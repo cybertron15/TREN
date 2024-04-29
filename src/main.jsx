7658cb42391d7bf905dfe95f31e40481e0c20e43
Palash Dhavle <palashdhavle15@gmail.com>
2024-04-29 23:31:26 +0530

    chore: removed unneccesary compnents from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App.tsx'
 import './index.css'
-import '@radix-ui/themes/styles.css';
-import './resorces/loading-bar/dist/loading-bar.js'
-import './resorces/loading-bar/dist/loading-bar.css'
-
-import { Theme, ThemePanel } from '@radix-ui/themes';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Theme>
     <App />
-    </Theme>
   </React.StrictMode>,
 )
